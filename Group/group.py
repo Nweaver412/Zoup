aa2f403ffb5e3db6d79360f1e821562ff58a6de1
Nate Weaver <97641419+Nweaver412@users.noreply.github.com>
2024-02-13 13:00:56 -0700

    Comments + new capabilities, still need to add capabilities when using specific groups

--- Group/group.py
+++ Group/group.py
@@ -1,17 +1,142 @@
 """
 Initializing data types for most common group types
 
-
 """
+from itertools import permutations
+import math
+
 from itertools import permutations
 
 class SymmetricGroup:
     def __init__(self, n):
         self.n = n
         self.elements = list(permutations(range(1, n + 1)))
+        self.elements_by_cycles = [(perm, self.cycle_notation(perm)) for perm in self.elements]
 
     def list(self):
+        """
+        Return list of groups within a group.
+
+        Returns:
+        - List of groups.
+        """
         return self.elements
+
+    def parse_cycle_notation(self, cycle_notation):
+        """
+        Parse a cycle notation string into a tuple representing the permutation.
+
+        Parameters:
+        - cycle_notation (str): A cycle notation string.
+
+        Returns:
+        - tuple: A tuple representing the permutation.
+        """
+        cycles = cycle_notation.split(" ")
+        perm = []
+        for cycle in cycles:
+            cycle = cycle.strip('()')
+            elements = list(map(int, cycle.split(',')))
+            perm.extend(elements)
+        return tuple(perm)
+
+    def cycle_notation(self, perm):
+        """
+        Convert a permutation tuple into a cycle notation string.
+
+        Parameters:
+        - perm (tuple): A permutation represented as a tuple.
+
+        Returns:
+        - str: A cycle notation string.
+        """
+        cycles = []
+        visited = set()
+        for i in perm:
+            if i not in visited:
+                cycle = [i]
+                visited.add(i)
+                next_element = perm[i - 1]
+                while next_element != i:
+                    cycle.append(next_element)
+                    visited.add(next_element)
+                    next_element = perm[next_element - 1]
+                cycles.append(tuple(cycle))
+        return " ".join([f"({','.join(map(str, cycle))})" for cycle in cycles])
+
+    def _count_transpositions(self, perm_list):
+        """
+        Count the number of transpositions in a permutation.
+
+        Parameters:
+        - perm_list (list): A list representation of a permutation.
+
+        Returns:
+        - int: The number of transpositions.
+        """
+        transpositions = 0
+        for i in range(self.n - 1):
+            if perm_list[i] > perm_list[i + 1]:
+                perm_list[i], perm_list[i + 1] = perm_list[i + 1], perm_list[i]
+                transpositions += 1
+        return transpositions
+
+    def sign(self, perm):
+        """
+        Calculate the sign of a permutation.
+
+        Parameters:
+        - perm (tuple): A permutation represented as a tuple.
+
+        Returns:
+        - int: 1 if the sign is positive, -1 if the sign is negative.
+        """
+        perm_list = list(perm)
+        transpositions = self._count_transpositions(perm_list)
+        return 1 if transpositions % 2 == 0 else -1
+
+    def order(self, perm):
+        """
+        Calculate the order (factorial) of the size of the group.
+        Utilizes a loop for values not in the precomputed dictionary.
+
+        Parameters:
+        - perm (tuple): A permutation represented as a tuple.
+
+        Returns:
+        - int: The factorial of the size of the group.
+        """
+        val = self.n
+        if val <= 10:
+            fac = {1: 1, 2: 2, 3: 6, 4: 24, 5: 120, 6: 720, 7: 5040, 8: 40320, 9: 362880, 10: 3628800}
+            return fac[val]
+        else:
+            result = 1
+            transpositions = self._count_transpositions(list(perm))
+            for i in range(2, val + 1):
+                result *= i
+            return result
+
+    def abelian(self, perm):
+        """
+        Check to see if not empty or singleton set, thus determining
+        if is abelian.
+
+        Parameters:
+        - perm (tuple): A permutation represented as a tuple.
+
+        Returns:
+        - Bool: True if Abelian, False if not.
+        """
+        val = self.n
+        return True if val >= 2 else False
+
+    def __repr__(self):
+        return f"SymmetricGroup({self.n})"
+
+
+
+class CyclicGroup: # TODO
 
     def sign(): #TODO
         return
@@ -22,11 +147,10 @@ class SymmetricGroup:
     def abelian(): #TODO
         return
 
-    def __repr__(self):
-        return f"SymmetricGroup({self.n})"
-
 # Broken Generate elements function. 
 # TODO
+
+
 class DihedralGroup: 
     def __init__(self, n):
         self.n = n
--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from Group.group import SymmetricGroup
 from Group.group import DihedralGroup
 
 if __name__ == "__main__":
-    H = SymmetricGroup(3)
-    G = DihedralGroup(6)
-
-    print(G.list())
-
+    G = SymmetricGroup(5)
+    sigma = G.parse_cycle_notation("(1,3) (2,5,4)")
+    order_sigma = G.order(sigma)
+    print(G.sign(sigma))
+    print(order_sigma)
+ 
